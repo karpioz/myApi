faff57d832f6b067cb68b6d035f01bdc8a0fb556
karpioz <karpioz@gmail.com>
2018-12-12 12:04:03 +0000

    ADDED passport and Moongoos and database model

--- app.js
+++ app.js
@@ -4,15 +4,40 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var config = require('./config/database');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//var index = require('./routes/index');
+//var users = require('./routes/users');
+
+// database connection set up
+try{
+    mongoose.connect(config.database);
+}
+catch(exception e){
+    console.log(e.message);
+}
+
+//set engine routes
+var api = require('./routes/api');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+//setup for CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//Initialize passport
+app.use(passport.initialize());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +47,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/', function(req, res) {
+    res.send('Page under construction.');
+});
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
